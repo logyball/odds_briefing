f35da25d8f45352267ceec6d39f85c3714511174
loganballard <loganballard@gmail.com>
2020-10-15 12:12:21 -0700

    updating wrong ref

--- api_test.go
+++ api_test.go
@@ -11,7 +11,7 @@ import (
 	"os"
 	"testing"
 
-	odds "github.com/loganballard/odds_briefing/odds_api"
+	odds "github.com/loganballard/odds_briefing/oddsapi"
 	"github.com/stretchr/testify/assert"
 )
 
